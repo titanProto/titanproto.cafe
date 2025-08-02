76bdc8638e98fd610829c3417a5dd430b00ca0a3
Titan <TitanProto@pm.me>
2025-08-02 23:15:26 +0930

    Version 1.4.0-2

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var txt2 = 'she/her, they/them'
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 window.onload = function typeWriter() {
+  document.getElementById("eevee").src = eeveelutionsFiles[currentEevee]; /* to fix eevee */
   if (i < txt0.length) {
     document.getElementById("irlName").innerHTML += txt0.charAt(i);
     i++;
@@ -20,4 +21,78 @@ window.onload = function typeWriter() {
     y++;
   }
   setTimeout(typeWriter, speed);
+}
+
+// const eeveelutions = [
+//   ['eevee', '/icons/eeveelutions/eevee.png'], 
+//   ['vapoureon', '/icons/eeveelutions/vapoureon.png'], 
+//   ['jolteon', '/icons/eeveelutions/jolteon.png'], 
+//   ['flareon', '/icons/eeveelutions/flareon.png'], 
+//   ['espeon', '/icons/eeveelutions/espeon.png'], 
+//   ['umbreon', '/icons/eeveelutions/umbreon.png'], 
+//   ['leafeon', '/icons/eeveelutions/leafeon.png'], 
+//   ['glaceon', '/icons/eeveelutions/glaceon.png'], 
+//   ['sylveon', '/icons/eeveelutions/sylveon.png']
+// ];
+const eeveelutions = [
+  'eevee',
+  'vapoureon',
+  'jolteon',
+  'flareon',
+  'espeon',
+  'umbreon',
+  'leafeon', 
+  'glaceon', 
+  'sylveon',
+];
+const eeveelutionsFiles = {
+  'eevee': '/icons/eeveelutions/eevee.png', 
+  'vapoureon': '/icons/eeveelutions/vapoureon.png', 
+  'jolteon': '/icons/eeveelutions/jolteon.png', 
+  'flareon': '/icons/eeveelutions/flareon.png', 
+  'espeon': '/icons/eeveelutions/espeon.png', 
+  'umbreon': '/icons/eeveelutions/umbreon.png', 
+  'leafeon': '/icons/eeveelutions/leafeon.png', 
+  'glaceon': '/icons/eeveelutions/glaceon.png', 
+  'sylveon': '/icons/eeveelutions/sylveon.png'
+};
+if (document.cookie == '') {
+  var currentEevee = 'eevee';
+}
+var currentEevee = document.cookie;
+console.log(currentEevee);
+var v = (eeveelutions.indexOf(currentEevee));
+console.log(v);
+
+function changeImage() {
+  if (v >= 8) {
+    v = 0;
+  }
+  else {
+    v++;
+  }
+  currentEevee = eeveelutions[v]
+  console.log(eeveelutionsFiles[currentEevee])
+  document.getElementById("eevee").src = eeveelutionsFiles[currentEevee];
+  document.cookie = currentEevee
+  // console.log(document.cookie)
+}
+function spin3D(clicked_id) {
+  element = document.getElementById(clicked_id)
+  element.classList.add("spin3D")
+  element.addEventListener("animationend", animationListener, once=true, false)
+}
+
+function spin2D(clicked_id) {
+  element = document.getElementById(clicked_id)
+  element.classList.add("spin2D")
+  element.addEventListener("animationend", animationListener, once=true, false)
+}
+
+function animationListener(event) {
+  switch (event.type) {
+    case "animationend":
+      element.classList.remove("spin3D")
+      element.classList.remove("spin2D")
+  }
 }
