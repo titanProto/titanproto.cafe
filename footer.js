0e86b857b46b8329cd484db21202a2437811782a
titanProto <TitanProto@pm.me>
2025-11-19 12:30:17 +1030

    version 1.4.4

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
-const footer = document.createElement('template');
+const footer = document.createElement("template");
 
 footer.innerHTML = `
     <div id="contacts">
@@ -16,7 +16,7 @@ footer.innerHTML = `
         <a href="https://www.last.fm/user/titanproto" target="_blank" class="contactButtons">last.fm</a>
     </div>
     <div id="version">
-        <p><a href="/">titanproto.cafe</a> 1.4.3</p>
+        <p><a href="/">titanproto.cafe</a> 1.4.4</p>
     </div>
     <div id="badgeDiv">
         <a href="https://ari.titanproto.cafe"><img class="badges" src="https://ari.titanproto.cafe/badges/titanproto.gif"></a>
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var i = 0;
 var x = 0;
 var y = 0;
-var txt0 = 'ari'; /* The text */
-var txt1 = 'titan.'
-var txt2 = 'she/her, they/them'
+var txt0 = "ari"; /* The text */
+var txt1 = "titan.";
+var txt2 = "she/her, they/them";
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 window.onload = function typeWriter() {
-  document.getElementById("eevee").src = eeveelutionsFiles[currentEevee]; /* to fix eevee */
-  if (vats == 0) { /* to fix fnv */
-    document.getElementById("fnv").src = "/icons/fnv_ranger.png"; 
+  document.getElementById("eevee").src =
+    eeveelutionsFiles[currentEevee]; /* to fix eevee */
+  if (vats == 0) {
+    /* to fix fnv */
+    document.getElementById("fnv").src = "icons/fnv_ranger.png";
   } else {
-    document.getElementById("fnv").src = "/icons/fnv_ranger_vats.png"; 
+    document.getElementById("fnv").src = "icons/fnv_ranger_vats.png";
   }
   if (i < txt0.length) {
     document.getElementById("irlName").innerHTML += txt0.charAt(i);
@@ -26,7 +28,7 @@ window.onload = function typeWriter() {
     y++;
   }
   setTimeout(typeWriter, speed);
-}
+};
 
 function setCookie(cname, cvalue) {
   document.cookie = cname + "=" + cvalue + ";";
@@ -34,10 +36,10 @@ function setCookie(cname, cvalue) {
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let b = 0; b <ca.length; b++) {
+  let ca = decodedCookie.split(";");
+  for (let b = 0; b < ca.length; b++) {
     let c = ca[b];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -48,56 +50,55 @@ function getCookie(cname) {
 }
 
 // const eeveelutions = [
-//   ['eevee', '/icons/eeveelutions/eevee.png'], 
-//   ['vapoureon', '/icons/eeveelutions/vapoureon.png'], 
-//   ['jolteon', '/icons/eeveelutions/jolteon.png'], 
-//   ['flareon', '/icons/eeveelutions/flareon.png'], 
-//   ['espeon', '/icons/eeveelutions/espeon.png'], 
-//   ['umbreon', '/icons/eeveelutions/umbreon.png'], 
-//   ['leafeon', '/icons/eeveelutions/leafeon.png'], 
-//   ['glaceon', '/icons/eeveelutions/glaceon.png'], 
+//   ['eevee', '/icons/eeveelutions/eevee.png'],
+//   ['vapoureon', '/icons/eeveelutions/vapoureon.png'],
+//   ['jolteon', '/icons/eeveelutions/jolteon.png'],
+//   ['flareon', '/icons/eeveelutions/flareon.png'],
+//   ['espeon', '/icons/eeveelutions/espeon.png'],
+//   ['umbreon', '/icons/eeveelutions/umbreon.png'],
+//   ['leafeon', '/icons/eeveelutions/leafeon.png'],
+//   ['glaceon', '/icons/eeveelutions/glaceon.png'],
 //   ['sylveon', '/icons/eeveelutions/sylveon.png']
 // ];
 const eeveelutions = [
-  'eevee',
-  'vapoureon',
-  'jolteon',
-  'flareon',
-  'espeon',
-  'umbreon',
-  'leafeon', 
-  'glaceon', 
-  'sylveon',
+  "eevee",
+  "vapoureon",
+  "jolteon",
+  "flareon",
+  "espeon",
+  "umbreon",
+  "leafeon",
+  "glaceon",
+  "sylveon",
 ];
 const eeveelutionsFiles = {
-  'eevee': '/icons/eeveelutions/eevee.png', 
-  'vapoureon': '/icons/eeveelutions/vapoureon.png', 
-  'jolteon': '/icons/eeveelutions/jolteon.png', 
-  'flareon': '/icons/eeveelutions/flareon.png', 
-  'espeon': '/icons/eeveelutions/espeon.png', 
-  'umbreon': '/icons/eeveelutions/umbreon.png', 
-  'leafeon': '/icons/eeveelutions/leafeon.png', 
-  'glaceon': '/icons/eeveelutions/glaceon.png', 
-  'sylveon': '/icons/eeveelutions/sylveon.png'
+  eevee: "icons/eeveelutions/eevee.png",
+  vapoureon: "icons/eeveelutions/vapoureon.png",
+  jolteon: "icons/eeveelutions/jolteon.png",
+  flareon: "icons/eeveelutions/flareon.png",
+  espeon: "icons/eeveelutions/espeon.png",
+  umbreon: "icons/eeveelutions/umbreon.png",
+  leafeon: "icons/eeveelutions/leafeon.png",
+  glaceon: "icons/eeveelutions/glaceon.png",
+  sylveon: "icons/eeveelutions/sylveon.png",
 };
-if (getCookie('eeveeCookie') != '') {
-  var currentEevee = getCookie('eeveeCookie')
+if (getCookie("eeveeCookie") != "") {
+  var currentEevee = getCookie("eeveeCookie");
 } else {
-  setCookie('eeveeCookie', 'eevee')
-  var currentEevee = getCookie('eeveeCookie')
+  setCookie("eeveeCookie", "eevee");
+  var currentEevee = getCookie("eeveeCookie");
 }
-var v = (eeveelutions.indexOf(currentEevee));
+var v = eeveelutions.indexOf(currentEevee);
 
-if (getCookie('vatsCookie') != '') {
-  var vats = getCookie('vatsCookie')
+if (getCookie("vatsCookie") != "") {
+  var vats = getCookie("vatsCookie");
 } else {
-  setCookie('vatsCookie', 0)
-  var vats = getCookie('vatsCookie')
+  setCookie("vatsCookie", 0);
+  var vats = getCookie("vatsCookie");
 }
-
 
 function changeImage(clicked_id) {
-  if (clicked_id == 'eevee') {
+  if (clicked_id == "eevee") {
     if (v >= 8) {
       v = 0;
     } else {
@@ -105,37 +106,48 @@ function changeImage(clicked_id) {
     }
     currentEevee = eeveelutions[v];
     document.getElementById("eevee").src = eeveelutionsFiles[currentEevee];
-    setCookie('eeveeCookie', currentEevee);
-  } else if (clicked_id == 'fnv') {
-    let vatsSound = new Audio('/assets/Fallout_Vats.wav');
+    setCookie("eeveeCookie", currentEevee);
+  } else if (clicked_id == "fnv") {
+    let vatsSound = new Audio("assets/Fallout_Vats.wav");
     vatsSound.volume = 0.32;
     vatsSound.play();
     if (vats == 0) {
-      document.getElementById("fnv").src = "/icons/fnv_ranger_vats.png";
-      vats = 1
+      document.getElementById("fnv").src = "icons/fnv_ranger_vats.png";
+      vats = 1;
     } else if (vats == 1) {
-      document.getElementById("fnv").src = "/icons/fnv_ranger.png";
-      vats = 0
+      document.getElementById("fnv").src = "icons/fnv_ranger.png";
+      vats = 0;
     }
-    setCookie('vatsCookie', vats);
+    setCookie("vatsCookie", vats);
   }
-} 
+}
 function spin3D(clicked_id) {
-  element = document.getElementById(clicked_id)
-  element.classList.add("spin3D")
-  element.addEventListener("animationend", animationListener, once=true, false)
+  element = document.getElementById(clicked_id);
+  element.classList.add("spin3D");
+  element.addEventListener(
+    "animationend",
+    animationListener,
+    (once = true),
+    false,
+  );
 }
 
 function spin2D(clicked_id) {
-  element = document.getElementById(clicked_id)
-  element.classList.add("spin2D")
-  element.addEventListener("animationend", animationListener, once=true, false)
+  element = document.getElementById(clicked_id);
+  element.classList.add("spin2D");
+  element.addEventListener(
+    "animationend",
+    animationListener,
+    (once = true),
+    false,
+  );
 }
 
 function animationListener(event) {
   switch (event.type) {
     case "animationend":
-      element.classList.remove("spin3D")
-      element.classList.remove("spin2D")
+      element.classList.remove("spin3D");
+      element.classList.remove("spin2D");
   }
 }
+
